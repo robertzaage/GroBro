c827190ea68e9ab85c100d721b388a0d8d47f9ad
Georg Welzel <gwelzel@mailbox.org>
2025-05-03 09:42:37 +0200

    ha: fix typo in password config
    Co-authored-by: Joey Lauterbach <77567363+jojeji@users.noreply.github.com>

--- grobro/ha/client.py
+++ grobro/ha/client.py
@@ -10,8 +10,9 @@ class Client:
 
     def __init__(self, host:str, port:str, tls:bool, user:str|None, password:str|None):
         # Setup target MQTT client for publishing
+        print(f"Connect to target mqtt '{host}:{port}'")
         self.client = mqtt.Client(client_id="grobro-ha")
-        if user and pasword:
+        if user and password:
             self.client.username_pw_set(user, password)
         if tls:
             self.client.tls_set(cert_reqs=ssl.CERT_NONE)
