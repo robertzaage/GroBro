4b6cbc75c4fbd6075e28840e8983fd758d541276
FBlass <felixblass@gmx.de>
2025-10-13 07:15:23 +0200

    Continue with local message processing if cloud forwarding fails (#127)
    * Continue with local message processing if cloud forwarding fails
    * Add debug local on_connect to HA server

--- grobro/grobro/client.py
+++ grobro/grobro/client.py
@@ -120,7 +120,7 @@ class Client:
             LOG.warning("Sending failed: %s", result)
 
     def __on_connect(self, client, userdata, flags, reason_code, properties):
-      LOG.debug(f"Connected with result code {reason_code}")
+      LOG.debug(f"Connected to GroBro MQTT server with result code {reason_code}")
       self._client.subscribe("c/#")      
 
     def __on_message(self, client, userdata, msg: MQTTMessage):
@@ -138,13 +138,16 @@ class Client:
             device_id = msg.topic.split("/")[-1]
             if GROWATT_CLOUD_ENABLED:
                 if GROWATT_CLOUD == "true" or device_id in GROWATT_CLOUD_FILTER:
-                    forward_client = self.__connect_to_growatt_server(device_id)
-                    forward_client.publish(
-                        msg.topic,
-                        payload=msg.payload,
-                        qos=msg.qos,
-                        retain=msg.retain,
-                    )
+                    try:
+                        forward_client = self.__connect_to_growatt_server(device_id)
+                        forward_client.publish(
+                            msg.topic,
+                            payload=msg.payload,
+                            qos=msg.qos,
+                            retain=msg.retain,
+                        )
+                    except Exception as e:
+                        LOG.error(f"Forwarding to GROWATT_CLOUD failed: {e}")
 
             unscrambled = parser.unscramble(msg.payload)
             LOG.debug(f"Received: %s %s", msg.topic, unscrambled.hex(" "))
